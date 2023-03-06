2b645e467d202ae25bfb75ce156d07c76de68f61
dancho393 <danez7000@gmail.com>
2023-03-06 14:07:08 +0200

    Initial commit

---
+++ src/main/java/com/example/upgit/UpGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.upgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpGitApplication.class, args);
+    }
+
+}
